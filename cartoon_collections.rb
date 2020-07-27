d10fc030ac1c6be10e05ac8d897f2b42d72def01
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-27 19:38:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
   array.each do |element|
     element.class == String
-  p array
+  p element
 end
 
 def list_dwarves(array)
